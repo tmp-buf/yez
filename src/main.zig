cf6b5e19b8e5da3e09bf20564e89d83de256b872
tmp-buf <>
2022-01-17 09:15:06 -0300

    Document, reformat and optimize
    Easily reaches 5GB/s of throughput on my machine

--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,19 @@ const std = @import("std");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const alloc = gpa.allocator();
+    const allocator = gpa.allocator();
     defer {
-        if (gpa.deinit()) {
-            @panic("yes: Leak detected.");
-        }
+        if (gpa.deinit())
+            @panic("yez: Leak detected.");
     }
 
-    var buf: []u8 = try alloc.alloc(u8, std.mem.page_size);
-    std.mem.copy(u8, buf, "y\n");
+    const str = "z\n";
+
+    // Page-aligned memory is what brings speed
+    var buf: []u8 = try allocator.alloc(u8, std.mem.page_size * 2);
+
+    // Fill buffer with string
+    std.mem.copy(u8, buf, str ** (std.mem.page_size * 2 / str.len));
 
     const stdout = std.io.getStdOut().writer();
     while (true) {
